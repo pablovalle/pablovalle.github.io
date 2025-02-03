f0157b380442b7e5c81a5092396f3d4ef5710655
Pablo Valle <pvalle@mondragon.edu>
2025-02-03 14:38:09 +0100

    portfolio edited

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Pablo Valle",
   title: "Hi all, I'm Pablo",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
